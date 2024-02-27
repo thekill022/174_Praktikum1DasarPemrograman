c7bfda0fff74cd36e15a3244000c90f549006ceb
thekill022 <gibfathoni@gmail.com>
2024-02-27 19:56:12 +0700

    Tambah Looping dan Cinditional Statement

--- Praktikum1DasarPemrograman/Praktikum1DasarPemrograman.cpp
+++ Praktikum1DasarPemrograman/Praktikum1DasarPemrograman.cpp
@@ -2,25 +2,43 @@
 using namespace std;
 
 int main()
-{
-	int nAlas, nTinggi;
-	float nLuas;
-	cout << "Masukkan Alasnya = ";
-	cin >> nAlas;
-	cout << "Masukkan Tingginya = ";
-	cin >> nTinggi;
-		nLuas = 0.5 * nAlas * nTinggi;
-		cout << "Luas Segitiga Adalah " << nLuas << endl;
+{	
+	bool patokan = true;
+	while (patokan)
+	{
+		int chocie;
+		cout << "Pilih Ingin Mencari Luas Apa\n 1. Segitiga\n 2. Persegi Panjang\n 3. Exit\n Masukkan Input : ";
+		cin >> chocie;
+		if (chocie == 1)
+		{
+			float nAlas, nTinggi;
+			float nLuas;
+			cout << "Program Luas Segitiga\n";
+			cout << "Masukkan Alasnya = ";
+			cin >> nAlas;
+			cout << "Masukkan Tingginya = ";
+			cin >> nTinggi;
+			nLuas = 0.5 * nAlas * nTinggi;
+			cout << "Luas Segitiga Adalah " << nLuas << endl;
+		}
+		else if (chocie == 2) {
+			int panjang, lebar, luas;
+			cout << "Program Luas Persegi Panjang\n";
+			cout << "Masukkan Panjang = ";
+			cin >> panjang;
+			cout << "masukkan Lebar = ";
+			cin >> lebar;
+			luas = panjang * lebar;
 
-		int panjang, lebar, luas;
-		cout << "Masukkan Panjang";
-		cin >> panjang;
-		cout << < "masukkan Lebar";
-		cin >> lebar;
-		luas = panjang * lebar;
-
-		cout << "Luasnya adalah = " << luas << endl;
-		
+			cout << "Luasnya adalah = " << luas << endl;
+		}
+		else if (chocie == 3) {
+			patokan = false;
+		}
+		else {
+			cout << "input Anda salah" << endl;
+		}
+	}	
 		//ini biar terminal tidak langsung close saat sistem di run
 		system("pause");
 }
